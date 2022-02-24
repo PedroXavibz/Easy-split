926977cc85346e0fa4c5a296ef7e864bcbd92298
Pedro Xavier <joao.pedro.xavibz@gmail.com>
2022-02-24 13:44:15 -0300

    Global themes(reset default css behavior) ✔

---
+++ src/styles/global/index.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	*,
+	*::before,
+	*::after {
+  	margin: 0;
+  	padding: 0;
+  	box-sizing: border-box;
+	}
+
+
+	:root {
+		font-size: 62.5%;
+	}
+
+	body {
+		background: ${props => props.theme.color.background};
+		line-height: 1.8;
+	}
+
+	ul{
+		list-style: none;
+		list-style-position: inside;
+	}
+
+	a,
+	a:active{
+		color: #ffff;
+		text-decoration: none;
+	}
+`;
+
+export default GlobalStyle;
