30bc1a8b54380a21e39aa5b79090176de996f2c7
Pedro Xavier <joao.pedro.xavibz@gmail.com>
2022-02-25 18:24:38 -0300

    Refactoring 🛠

--- src/components/text/index.jsx
+++ src/components/text/index.jsx
@@ -1,14 +1,7 @@
-import parse from 'html-react-parser';
 import ContainerText from './style';
 
-const Text = ({ children, wordsToHighligth }) => {
-  let textReplaced = children;
-
-  wordsToHighligth.forEach(word => {
-    textReplaced = textReplaced.replaceAll(word, match => `<span>${match}</span>`);
-  });
-
-  return <ContainerText>{parse(textReplaced)}</ContainerText>;
+const Text = ({ children }) => {
+  return <ContainerText>{children}</ContainerText>;
 };
 
 export default Text;
