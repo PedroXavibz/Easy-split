c5af1a49b3610803ce83dc6434fc65f1001e19a1
Pedro Xavier <joao.pedro.xavibz@gmail.com>
2022-02-25 16:44:47 -0300

    Added main-section component ✔

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/themes';
 import GlobalStyle from './styles/global';
 import Header from './components/header';
+import MainSection from './components/main-section';
+import Context from './style';
 
 const App = () => (
   <ThemeProvider theme={theme}>
-    <div className='App'>
-      <GlobalStyle />
-      <Header />
-    </div>
+    <GlobalStyle />
+    <Header />
+    <main>
+      <Context>
+        <MainSection />
+      </Context>
+    </main>
   </ThemeProvider>
 );
 
