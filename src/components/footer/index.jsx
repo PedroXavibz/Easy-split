b622f8524baa01bfc0609d25748823fd10f2ea9d
Pedro Xavier <joao.pedro.xavibz@gmail.com>
2022-02-25 18:28:41 -0300

    Footer structure and stylization ✔️

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,12 @@
+import ContainerFooter from './style';
+import Context from '../../style';
+
+const Footer = () => (
+  <ContainerFooter>
+    <Context>
+      <h4>© 2021 easy split</h4>
+    </Context>
+  </ContainerFooter>
+);
+
+export default Footer;
